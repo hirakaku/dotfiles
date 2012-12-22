67f1fde0baf8c6299eb3cbd13c43a79c7ec09e4c
hirakaku <hirakaku@gmail.com>
2012-12-22 17:47:02 +0900

    add mklink.bat

---
+++ mklink.bat
@@ -0,0 +1,10 @@
+@echo mklink written by hirakaku
+
+mklink .gitconfig dotfiles\_gitconfig
+mklink .nodoka dotfiles\dot.nodoka
+mklink .tmuxrc dotfiles\tmux.conf
+mklink .zshrc dotfiles\_zshrc
+
+mklink .vimrc dotfiles\_vimrc
+mklink .gvimrc dotfiles\_gvimrc
+mklink /D .vim dotfiles\vimfiles
